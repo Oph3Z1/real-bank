881f28bfc1ff6b6a1e31a97e10e169a655b8dc7c
Oph3Z <59979752+Oph3Z1@users.noreply.github.com>
2023-12-09 14:09:39 +0100

    1.0

---
+++ html/app.js
@@ -0,0 +1,71 @@
+const store = Vuex.createStore({
+    state: {},
+    mutations: {},
+    actions: {}
+});
+
+const app = Vue.createApp({
+    data: () => ({
+        show: true,
+        CurrentScreen: 'BankScreen', // 'Password' - 'BankScreen'
+        CurrentMenu: 'Dashboard',
+        CardStyle: 1, // '1' - '2'
+    }),
+
+    methods: {
+
+    },  
+    computed: {
+        
+    },
+
+    watch: {
+    
+    },
+
+    beforeDestroy() {
+        window.removeEventListener('keyup', this.onKeyUp);
+    },
+
+    mounted() {
+        window.addEventListener("message", event => {
+            window.addEventListener('keyup', this.onKeyUp);
+            switch (event.data.message) {
+                case "OPEN":
+                    this.Show = true
+                break;
+                
+                case "CLOSE":
+                    this.Show = false
+                break;
+            }   
+        });
+    },
+    
+});
+
+app.use(store).mount("#app");
+
+const resourceName = window.GetParentResourceName ? window.GetParentResourceName() : "real-bank";
+
+window.postNUI = async (name, data) => {
+    try {
+        const response = await fetch(`https://${resourceName}/${name}`, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(data)
+        });
+        return !response.ok ? null : response.json();
+    } catch (error) {
+        // console.log(error)
+    }
+};
+
+
